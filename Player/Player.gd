2f944d2986ef3e097eb89a42cbfcde4bcdb1e063
EvilYep <guillaume.moneret@gmail.com>
2023-03-07 08:25:48 +0100

    using built-in animation tree and state machine :herb:

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,11 +2,15 @@ extends KinematicBody2D
 
 class_name Player
 
+onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var animation_tree: AnimationTree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 const ACCELERATION := 500
 const MAX_SPEED := 100
 const FRICTION = 500
 
-var moving_direction := Vector2.ZERO setget set_moving_direction, get_moving_direction
+var direction := Vector2.ZERO setget set_direction, get_direction
 var velocity : Vector2
 
 #### ACCESSORS ####
@@ -14,10 +18,10 @@ var velocity : Vector2
 func is_class(value: String): return value == "Player" or .is_class(value)
 func get_class() -> String: return "Player"
 
-func set_moving_direction(value: Vector2) -> void:
-	if moving_direction != value:
-		moving_direction = value
-func get_moving_direction() -> Vector2: return moving_direction
+func set_direction(value: Vector2) -> void:
+	if direction != value:
+		direction = value
+func get_direction() -> Vector2: return direction
 
 #### BUILT-IN ####
 
@@ -29,14 +33,18 @@ func _input(_event: InputEvent) -> void:
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
-	set_moving_direction(dir.limit_length())
-
-func _physics_process(delta: float) -> void:
-	if moving_direction != Vector2.ZERO:
-		velocity = velocity.move_toward(moving_direction * MAX_SPEED, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	set_direction(dir.limit_length())
 	
+	if direction != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", direction)
+		animation_tree.set("parameters/Run/blend_position", direction)
+		animation_state.travel("Run")
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	else:
+		animation_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+
+func _physics_process(_delta: float) -> void:
 	var __ = move_and_slide(velocity)
 
 #### VIRTUALS ####
