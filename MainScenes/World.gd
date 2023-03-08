b28ff6434a9f171e119a66cbd66b32353c4555e8
EvilYep <guillaume.moneret@gmail.com>
2023-03-08 02:07:16 +0100

    autotiling more and cool AF water shader :ocean:

---
+++ MainScenes/World.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+export(OpenSimplexNoise) var perlin_noise
+
+#### ACCESSORS ####
+
+#### BUILT-IN ####
+
+func _ready() -> void:
+	pass
+
+#### VIRTUALS ####
+
+#### LOGIC ####
+
+#### INPUTS ####
+
+#### SIGNAL RESPONSES ####
