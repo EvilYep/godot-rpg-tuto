3969620c0fdece07b4b9946ce8b9689aa2d0e89d
EvilYep <guillaume.moneret@gmail.com>
2023-03-08 07:54:58 +0100

    hitboxes and hurtboxes - layer & mask :crossed_swords:

--- World/Grass.gd
+++ World/Grass.gd
@@ -3,11 +3,11 @@ extends Node2D
 var grass_FX_scene = preload("res://World/GrassEffect.tscn")
 var grass_FX
 
-func _ready() -> void:
+func create_grass_FX() -> void:
 	grass_FX = grass_FX_scene.instance()
 	grass_FX.set_position(self.position)
+	get_tree().current_scene.add_child(grass_FX)
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("attack"):
-		get_tree().current_scene.add_child(grass_FX)
-		queue_free()
+func _on_HurtBox_area_entered(_area: Area2D) -> void:
+	create_grass_FX()
+	queue_free()
